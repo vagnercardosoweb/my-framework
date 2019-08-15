98365414dea7f99e33b84cbc7f2e428ced85fd7f
Vagner Cardoso <vagnercardosoweb@gmail.com>
2019-08-15 03:27:27 -0300

    Melhorias nas funções do core.

--- source/helpers.php
+++ source/helpers.php
@@ -5,7 +5,7 @@
  *
  * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
  * @license http://www.opensource.org/licenses/mit-license.html MIT License
- * @copyright 03/08/2019 Vagner Cardoso
+ * @copyright 15/08/2019 Vagner Cardoso
  */
 
 use Core\App;
