c921b84dc49dbdadb8a6324485637e27105f1e99
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-02-28 01:42:17 -0300

    fix: retornava valor errado pois o trim mudava quando não era uma string.
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- source/Env.php
+++ source/Env.php
@@ -6,7 +6,7 @@
  * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
  * @link https://github.com/vagnercardosoweb
  * @license http://www.opensource.org/licenses/mit-license.html MIT License
- * @copyright 26/02/2020 Vagner Cardoso
+ * @copyright 28/02/2020 Vagner Cardoso
  */
 
 namespace Core;
@@ -94,7 +94,7 @@ class Env
             return $matches[2];
         }
 
-        return trim($value);
+        return is_string($value) ? trim($value) : $value;
     }
 
     /**
