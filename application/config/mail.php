5ce36df87f1f3f99a061e2227790a8c469229c89
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-07-05 16:50:58 -0300

    chore: melhorias nas configurações de emails.

--- application/config/mail.php
+++ application/config/mail.php
@@ -6,10 +6,18 @@
  * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
  * @link https://github.com/vagnercardosoweb
  * @license http://www.opensource.org/licenses/mit-license.html MIT License
- * @copyright 20/05/2020 Vagner Cardoso
+ * @copyright 05/07/2020 Vagner Cardoso
  */
 
 use Core\Env;
+
+$clientConfig = require __DIR__.'/client.php';
+
+$mailFromMail = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
+
+if ('localhost' === $mailFromMail) {
+    $mailFromMail .= '.dev';
+}
 
 return [
     'debug' => Env::get('MAIL_DEBUG', 0), // 0 | 1 | 2
@@ -21,8 +29,8 @@ return [
     'username' => Env::get('MAIL_USER', null),
     'password' => Env::get('MAIL_PASS', null),
     'from' => [
-        'name' => Env::get('MAIL_FROM_NAME', null),
-        'mail' => Env::get('MAIL_FROM_MAIL', 'no-reply@localhost.dev'),
+        'name' => Env::get('MAIL_FROM_NAME', $clientConfig['name']),
+        'mail' => Env::get('MAIL_FROM_MAIL', "no-reply@{$mailFromMail}"),
     ],
     'language' => [
         'code' => Env::get('MAIL_LANGUAGE_CODE', null),
