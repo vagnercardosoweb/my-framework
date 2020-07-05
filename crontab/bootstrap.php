d06416fea6e859a4b4908c3e2baf802e964ecc0a
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-07-05 06:44:15 -0300

    feat: adiciona pasta para crontabs.

---
+++ crontab/bootstrap.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * VCWeb Networks <https://www.vcwebnetworks.com.br/>
+ *
+ * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
+ * @link https://github.com/vagnercardosoweb
+ * @license http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright 05/07/2020 Vagner Cardoso
+ */
+
+use Core\App;
+
+try {
+    // Constants
+    define('ROOT', realpath(dirname(__DIR__)));
+    define('PUBLIC_FOLDER', __DIR__.'/../public_html');
+    define('APP_FOLDER', __DIR__.'/../application');
+    define('RESOURCE_FOLDER', APP_FOLDER.'/resources');
+    define('BASE_URL', 'http://localhost');
+
+    // Autoload
+    require_once APP_FOLDER.'/vendor/autoload.php';
+
+    // Loader app
+    $app = App::getInstance();
+    $app->registerProviders();
+    $app->registerEvents();
+} catch (\Exception $e) {
+    die("ERROR: {$e->getMessage()}\n");
+}
---
+++ crontab/test.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * VCWeb Networks <https://www.vcwebnetworks.com.br/>
+ *
+ * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
+ * @link https://github.com/vagnercardosoweb
+ * @license http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright 05/07/2020 Vagner Cardoso
+ */
+
+require_once __DIR__.'/bootstrap.php';
+
+echo "Cron started successfully.\n";
